9c8ba736bb60111c3f4061cd468768c1544d7f97
Fardullah01 <mdfardullah001@gmail.com>
2026-05-12 11:36:52 +0600

    variable and datatype completed

---
+++ 03-datatypeandvariable/Program.cs
@@ -0,0 +1,14 @@
+﻿int a=10;
+int b=20;
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+double div=(double)a/b; //typecasting
+Console.WriteLine("Sum :"+sum+"\nSub :"+sub+"\nMul :"+mul+"\nDiv :"+div);
+string name="fardullah";
+char grade='A';
+bool isPassed=true;
+Console.WriteLine("Name :"+name+"\nGrade :"+grade+"\nIs Passed :"+isPassed);
+double pi=3.14;
+float e=2.71f;
+Console.WriteLine("Pi :"+pi+"\nE :"+e+"\nsum : "+(pi+e)); 
